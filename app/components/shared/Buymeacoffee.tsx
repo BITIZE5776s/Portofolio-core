d89a4e0d708e36e3b18695055ac7d0224c9ba2e6
ZakariaMirinioui <ziko9172@gmail.com>
2026-08-25 22:28:21 +0100

    feat: add share buttons, contact CTA, and utility helpers

---
+++ app/components/shared/Buymeacoffee.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Buymeacoffee() {
+  return (
+    <a
+      href="mailto:zakmirinioui@gmail.com"
+      className="flex items-center lg:flex-row flex-col lg:justify-around justify-center lg:text-start text-center gap-2 min-h-[110px] dark:bg-primary-bg bg-secondary-bg hover:dark:bg-[#2e290e44] rounded-lg border dark:border-zinc-800 border-zinc-200 hover:dark:border-[#ffdd0060] hover:border-[#e6d14fe7] duration-300 p-6 group"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      Interested in working together? Let&apos;s connect!
+      <span className="dark:text-primary-color text-secondary-color font-semibold">
+        zakmirinioui@gmail.com
+      </span>
+    </a>
+  );
+}
