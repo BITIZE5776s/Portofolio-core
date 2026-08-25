170285ab295660a529a7183f7ee07d30b6567263
ZakariaMirinioui <ziko9172@gmail.com>
2026-08-25 22:26:56 +0100

    feat: define TypeScript types and Sanity schema definitions

---
+++ schemas/index.ts
@@ -0,0 +1,15 @@
+import job from "./job";
+import profile from "./profile";
+import project from "./project";
+import heroe from "./heroe";
+import blockContent from "./blockContent";
+
+export const schemaTypes = [
+  profile,
+  job,
+  project,
+  heroe,
+
+  // Reference types
+  blockContent,
+];
---
+++ types/index.ts
@@ -0,0 +1,54 @@
+import { PortableTextBlock } from "sanity";
+
+export type ProfileType = {
+  _id: string;
+  fullName: string;
+  headline: string;
+  profileImage: {
+    image: string;
+    lqip: string;
+    alt: string;
+  };
+  shortBio: string;
+  email: string;
+  fullBio: PortableTextBlock[];
+  location: string;
+  resumeURL: string;
+  og: string;
+  usage: PortableTextBlock[];
+};
+
+export type JobType = {
+  _id: string;
+  name: string;
+  jobTitle: string;
+  logo: string;
+  url: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+};
+
+export type ProjectType = {
+  _id: string;
+  name: string;
+  slug: string;
+  tagline: string;
+  projectUrl: string;
+  repository: string;
+  logo: string;
+  coverImage: {
+    image: string;
+    alt: string | null;
+    lqip: string;
+  };
+  description: PortableTextBlock[];
+};
+
+export type HeroeType = {
+  _id: string;
+  _createdAt: string;
+  name: string;
+  url: string;
+  met: boolean;
+};
