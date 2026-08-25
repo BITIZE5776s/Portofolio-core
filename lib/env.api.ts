eba1462672ccfc1c4442b2f5c81ec97d673ac907
ZakariaMirinioui <ziko9172@gmail.com>
2026-08-25 22:24:43 +0100

    feat: set up Sanity client and environment configuration

---
+++ lib/env.api.ts
@@ -0,0 +1,12 @@
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
+
+export const dataset: string =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+
+export const token = process.env.NEXT_PUBLIC_SANITY_ACCESS_TOKEN;
+
+export const hookSecret = process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET;
+export const mode = process.env.NODE_ENV;
+
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-07-21";
